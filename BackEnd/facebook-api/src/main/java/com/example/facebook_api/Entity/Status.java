ec44737fbf9c7638aa7dd8339fc56d0c6e31b160
Thanh Ma <thanhm963@gmail.com>
2025-04-26 11:58:56 +0700

    update postrepo

---
+++ BackEnd/facebook-api/src/main/java/com/example/facebook_api/Entity/Status.java
@@ -0,0 +1,62 @@
+package com.example.facebook_api.Entity;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+
+public class Status {
+    @PrimaryKey
+    private UUID statusID;
+
+    private UUID userID;
+    private String statusImageURL;
+    private Timestamp uploadTime;
+
+    public Status() {
+        super();
+    }
+
+    public Status(UUID statusID, UUID userID, String statusImageURL, Timestamp uploadTime) {
+        super();
+        this.statusID = statusID;
+        this.userID = userID;
+        this.statusImageURL = statusImageURL;
+        this.uploadTime = uploadTime;
+    }
+
+    public UUID getStatusID() {
+        return statusID;
+    }
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public String getStatusImageURL() {
+        return statusImageURL;
+    }
+
+    public Timestamp getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setStatusID(UUID statusID) {
+        this.statusID = statusID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+    public void setStatusImageURL(String statusImageURL) {
+        this.statusImageURL = statusImageURL;
+    }
+
+    public void setUploadTime(Timestamp uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+
+     
+    
+}
